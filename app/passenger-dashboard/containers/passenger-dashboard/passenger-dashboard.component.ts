721cc88c98e1c530e17c028bbbd2fd1da309c98f
Nguyen Phuc Thien An <npta97@gmail.com>
2018-06-23 22:29:43 +0700

    Immutable state changes

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -58,11 +58,18 @@ export class PassengerDashboardComponent implements OnInit {
     }];
   }
 
-  handleEdit(event) {
-    console.log(event);
+  handleEdit(event: Passenger) {
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+      return passenger;
+    })
   }
 
-  handleRemove(event) {
-    console.log(event);
+  handleRemove(event: Passenger) {
+    this.passengers = this.passengers.filter(
+      (passenger: Passenger) => passenger.id !== event.id
+    );
   }
 }
