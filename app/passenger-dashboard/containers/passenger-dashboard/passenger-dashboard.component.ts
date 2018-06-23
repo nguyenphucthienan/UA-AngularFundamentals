c221de2441d23309d3c93388f5f94d8a07b5d6f2
Nguyen Phuc Thien An <npta97@gmail.com>
2018-06-23 23:38:51 +0700

    Data Services and Dependency Injection

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PassengerDashboardService } from '../../passenger-dashboard.service';
+
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -25,40 +27,11 @@ export class PassengerDashboardComponent implements OnInit {
   logo: string = 'img/logo.svg';
   passengers: Passenger[];
 
-  constructor() { }
+  constructor(private passengerService: PassengerDashboardService) { }
+
 
   ngOnInit() {
-    this.passengers = [{
-      id: 1,
-      fullName: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: null
-    }, {
-      id: 2,
-      fullName: 'Rose',
-      checkedIn: false,
-      checkInDate: null,
-      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-    }, {
-      id: 3,
-      fullName: 'James',
-      checkedIn: true,
-      checkInDate: 1491606000000,
-      children: null
-    }, {
-      id: 4,
-      fullName: 'Louise',
-      checkedIn: true,
-      checkInDate: 1488412800000,
-      children: [{ name: 'Jessica', age: 1 }]
-    }, {
-      id: 5,
-      fullName: 'Tina',
-      checkedIn: false,
-      checkInDate: null,
-      children: null
-    }];
+    this.passengers = this.passengerService.getPassengers();
   }
 
   handleEdit(event: Passenger) {
--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -6,6 +6,8 @@ import { PassengerDashboardComponent } from './containers/passenger-dashboard/pa
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
+import { PassengerDashboardService } from './passenger-dashboard.service';
+
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
@@ -17,6 +19,9 @@ import { PassengerDetailComponent } from './components/passenger-detail/passenge
   ],
   exports: [
     PassengerDashboardComponent
+  ],
+  providers: [
+    PassengerDashboardService
   ]
 })
 export class PassengerDashboardModule { }
