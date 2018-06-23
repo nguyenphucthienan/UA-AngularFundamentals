b9bb8837784f301bb43d25522b1231cdb25aac89
Nguyen Phuc Thien An <npta97@gmail.com>
2018-06-23 23:50:02 +0700

    Understanding @Injectable

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,6 +1,12 @@
-import { Passenger } from "./models/passenger.interface";
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
+import { Passenger } from './models/passenger.interface';
+
+@Injectable()
 export class PassengerDashboardService {
+  constructor(private http: Http) { }
+
   getPassengers(): Passenger[] {
     return [{
       id: 1,
