9b0d3d2f0677b4ee7b4d416265aff51592d91ede
Nguyen Phuc Thien An <npta97@gmail.com>
2018-06-23 14:36:33 +0700

    Passing data into components with @Input

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -8,9 +8,8 @@ import { Passenger } from '../../models/passenger.interface';
   template: `
     <div>
       <img [src]="logo">
-      <passenger-count></passenger-count>
+      <passenger-count [items]="passengers"></passenger-count>
       <passenger-detail></passenger-detail>
-      <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
           <span 
